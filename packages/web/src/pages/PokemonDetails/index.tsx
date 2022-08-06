a01c943b68c9a9ca55989d981be46d179f945cea
Miguel Rios <miguelriosoliveira@gmail.com>
2022-08-06 04:30:56 +0200

    chore: Removed actions workflow

--- packages/web/src/pages/PokemonDetails/index.tsx
+++ packages/web/src/pages/PokemonDetails/index.tsx
@@ -14,7 +14,7 @@ export function PokemonDetails() {
 	const { pokemonId: id } = useParams();
 	const pokemonId = Number(id);
 
-	const [pokemon, setPokemon] = useState<PokemonData>({} as PokemonData);
+	const [pokemon, setPokemon] = useState<PokemonData | null>(null);
 
 	useEffect(() => {
 		const loadPokemon = async () => {
@@ -29,128 +29,124 @@ export function PokemonDetails() {
 		loadPokemon();
 	}, [pokemonId]);
 
-	if (Number.isNaN(pokemonId)) {
+	if (Number.isNaN(pokemonId) || !pokemon) {
 		return null;
 	}
 
 	const { common: commonEvolutions, variant: variantEvolutions } = pokemon.evolutionChain;
 
 	return (
-		pokemon && (
-			<div className={styles['pokemon-details-component']}>
-				<div className="header">
-					<Button
-						variant="contained"
-						// color="default"
-						startIcon={<ArrowBack />}
-						onClick={() => navigate(-1)}
-					>
-						Back
-					</Button>
+		<div className={styles['pokemon-details-component']}>
+			<div className="header">
+				<Button
+					variant="contained"
+					// color="default"
+					startIcon={<ArrowBack />}
+					onClick={() => navigate(-1)}
+				>
+					Back
+				</Button>
+			</div>
+
+			<div className="main">
+				<div className="sprite">
+					<Sprite name={pokemon.name} number={pokemon.number} />
 				</div>
 
-				<div className="main">
-					<div className="sprite">
-						<Sprite name={pokemon.name} number={pokemon.number} />
-					</div>
-
-					<div className="infos">
-						<Typography variant="h4" className="name">
-							{pokemon.name} #{pokemon.number}
-						</Typography>
-
-						<div className="description">
-							<Typography variant="body1" className="text--bold">
-								{pokemon.description}
-							</Typography>
-						</div>
-
-						<div className="types">
-							<div className="own">
-								<div className="title">
-									<Typography variant="body1" className="text--bold">
-										{pokemon.types.length === 1 ? 'TYPE' : 'TYPES'}
-									</Typography>
-								</div>
-								<div className="list">
-									{pokemon.types.map(type => (
-										<Typography
-											key={type}
-											align="center"
-											variant="overline"
-											className={`type type--${type}`}
-										>
-											{type}
-										</Typography>
-									))}
-								</div>
-							</div>
-
-							<div className="weaknesses">
-								<div className="title">
-									<ReportProblem />
-									<Typography variant="body1" className="text--bold">
-										WEAKNESSES
-									</Typography>
-									<ReportProblem />
-								</div>
-								<div className="list">
-									{pokemon.weaknesses.map(weakness => (
-										<Typography
-											key={weakness}
-											align="center"
-											variant="overline"
-											className={`type type--${weakness}`}
-										>
-											{weakness}
-										</Typography>
-									))}
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div className="evolution-chain">
-					<Typography variant="body1" className="title text--bold">
-						EVOLUTION CHAIN
+				<div className="infos">
+					<Typography variant="h4" className="name">
+						{pokemon.name} #{pokemon.number}
 					</Typography>
 
-					<div className="list">
-						<div className="common">
-							{commonEvolutions.map((poke, index) => {
-								return (
-									<div key={poke.displayName} className="pokemon-form">
-										<PokemonCard key={poke.number} pokemon={poke} />
-										{index === commonEvolutions.length - 1 &&
-										variantEvolutions.length === 0 ? null : (
-											<ArrowRight />
-										)}
-									</div>
-								);
-							})}
+					<div className="description">
+						<Typography variant="body1" className="text--bold">
+							{pokemon.description}
+						</Typography>
+					</div>
+
+					<div className="types">
+						<div className="own">
+							<div className="title">
+								<Typography variant="body1" className="text--bold">
+									{pokemon.types.length === 1 ? 'TYPE' : 'TYPES'}
+								</Typography>
+							</div>
+							<div className="list">
+								{pokemon.types.map(type => (
+									<Typography
+										key={type}
+										align="center"
+										variant="overline"
+										className={`type type--${type}`}
+									>
+										{type}
+									</Typography>
+								))}
+							</div>
 						</div>
 
-						{variantEvolutions.length > 0 && (
-							<div className="variant">
-								<div className="half">
-									{variantEvolutions
-										.slice(0, Math.floor(variantEvolutions.length / 2))
-										.map(poke => (
-											<PokemonCard key={poke.number} pokemon={poke} />
-										))}
-								</div>
-
-								<div className="half">
-									{variantEvolutions.slice(Math.ceil(variantEvolutions.length / 2)).map(poke => (
-										<PokemonCard key={poke.number} pokemon={poke} />
-									))}
-								</div>
+						<div className="weaknesses">
+							<div className="title">
+								<ReportProblem />
+								<Typography variant="body1" className="text--bold">
+									WEAKNESSES
+								</Typography>
+								<ReportProblem />
 							</div>
-						)}
+							<div className="list">
+								{pokemon.weaknesses.map(weakness => (
+									<Typography
+										key={weakness}
+										align="center"
+										variant="overline"
+										className={`type type--${weakness}`}
+									>
+										{weakness}
+									</Typography>
+								))}
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		)
+
+			<div className="evolution-chain">
+				<Typography variant="body1" className="title text--bold">
+					EVOLUTION CHAIN
+				</Typography>
+
+				<div className="list">
+					<div className="common">
+						{commonEvolutions.map((poke, index) => {
+							return (
+								<div key={poke.displayName} className="pokemon-form">
+									<PokemonCard key={poke.number} pokemon={poke} />
+									{index === commonEvolutions.length - 1 &&
+									variantEvolutions.length === 0 ? null : (
+										<ArrowRight />
+									)}
+								</div>
+							);
+						})}
+					</div>
+
+					{variantEvolutions.length > 0 && (
+						<div className="variant">
+							<div className="half">
+								{variantEvolutions.slice(0, Math.floor(variantEvolutions.length / 2)).map(poke => (
+									<PokemonCard key={poke.number} pokemon={poke} />
+								))}
+							</div>
+
+							<div className="half">
+								{variantEvolutions.slice(Math.ceil(variantEvolutions.length / 2)).map(poke => (
+									<PokemonCard key={poke.number} pokemon={poke} />
+								))}
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
 	);
 }
