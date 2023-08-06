a7211b0bd338200f47b1b5f721b8bf255418ecee
Miguel Rios <miguelriosoliveira@gmail.com>
2023-08-06 23:28:28 +0200

    fix: 404 error on Vercel

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react';
-import Unfonts from 'unplugin-fonts/vite';
+import Unfonts from 'unplugin-fonts';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	appType: 'mpa',
 	plugins: [
 		react(),
-		Unfonts({
+		Unfonts.vite({
 			google: {
 				families: [{ name: 'Roboto', styles: 'wght@400;500;700' }],
 			},
