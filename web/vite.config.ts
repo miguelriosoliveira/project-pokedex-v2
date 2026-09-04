dd01dd66adff593af31fc75a104425dd5199f04a
Miguel Rios <miguelriosoliveira@gmail.com>
2026-09-04 17:36:33 +0200

    chore: Flat packages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,7 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import Unfonts from 'unplugin-fonts';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
